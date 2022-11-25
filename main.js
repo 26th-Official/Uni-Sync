419b205d818cf459a5fb87dc50e0df1c5fb3424c
26th_Official <26thofficial.creator@gmail.com>
2022-11-25 20:33:18 +0530

    Trs

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 let urls=[]
 
 function logTabs(tabs) {
+    const element = document.getElementById("tab_table")
+
     for (const tab of tabs) {
       urls.push(tab.url)
+      const node = document.createTextNode(tab.url)
+      const tr = document.createElement("tr")
+      const div = document.createElement("div")
+      div.classList.add("active_tab")
+      const h5 = document.createElement("h5")
+
+      h5.appendChild(node)
+      div.appendChild(h5)
+      tr.appendChild(div)
+      
+      element.appendChild(tr)
     }
   }
   
 
-  
 browser.tabs.query({}).then(logTabs);
 
-
-
 console.log(urls)
+
+
+
+
+
 
 
 
