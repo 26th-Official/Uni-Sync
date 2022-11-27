692dce035c7b19b3ab7cf8295c16b2fcb445d508
26th-Official <you@example.com>
2022-11-27 09:13:01 +0530

    Trs

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
-import { MongoClient } from "mongodb";
+// import axios from 'axios';
+// import { MongoClient } from "mongodb";
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 
-const uri = "mongodb+srv://Sample:Qwerty123@uni-sync.ebaobdx.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// const uri = "mongodb+srv://Sample:Qwerty123@uni-sync.ebaobdx.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+var axios = require('axios');
+var data = JSON.stringify({
+    "collection": "Users",
+    "database": "UniSYNC",
+    "dataSource": "Uni-SYNC",
+    "projection": {
+        "_id": 1
+    }
+});
 
 let urls=[]
 function logTabs(tabs) {
@@ -36,15 +49,33 @@ function logTabs(tabs) {
       "URLs":urls
     }
 
-    console.log(obj)
-    client.connect(err => {
-      obj = {}
-      const collection = client.db("UniSYNC").collection("Users");
-      collection.insertOne(obj)
-      // perform actions on the collection object
-      client.close()
-    });
-  }
+    var config = {
+      method: 'post',
+      url: 'https://data.mongodb-api.com/app/data-kcqqe/endpoint/data/v1/action/insertOne',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Request-Headers': '*',
+        'api-key': 'ON7CfbL2PTB2I1RINIeWdwLgxu38aZY2g2ff21PNJZPxrFkCF8u866kAyuL8Akyx',
+      },
+      data: {
+        "collection":"Users",
+        "database":"UniSYNC",
+        "dataSource":"Uni-SYNC",
+        "document": obj
+      }
+  };
+              
+    axios(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    
+
+
+    }
   
 
 browser.tabs.query({}).then(logTabs);
